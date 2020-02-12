af135ccb87848e2f49cb7a61175ed1f2d5f8ee33
duyuqian <15738381799@163.com>
2020-02-12 22:17:27 +0800

    [duyuqian] feat: let each teacher adds five to each student he teaches and print out information of teachers and students

--- modifier/src/main/java/Teacher.java
+++ modifier/src/main/java/Teacher.java
@@ -27,4 +27,20 @@ public class Teacher {
     public void setStudents(ArrayList<Student> students) {
         this.students = students;
     }
+
+    public void setStudentsScore(Student student, int score) {
+        if (this.students.contains(student)) {
+            student.setScore(score);
+        } else {
+            System.out.println("He/She is not my student!");
+        }
+    }
+
+    public void printTeacherInfo() {
+        System.out.println("I am a teacher." + "My name is " + this.name + ".");
+        System.out.println("I teach these students:");
+        for (Student student : this.students) {
+            System.out.println("I am a student of " + this.name + ",My name is " + student.getName() + ".My score is " + student.getScore() + ".");
+        }
+    }
 }
